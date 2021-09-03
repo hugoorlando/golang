c191937113f80a85e161604929a64c1873d65d9a
happycodingpy <hugo.orlando2030@gmail.com>
2021-09-03 16:07:00 -0300

    hello world

--- go-programming/01-hello-world/main.go
+++ go-programming/01-hello-world/main.go
@@ -1,21 +1,11 @@
-// package main declaracion del nombre del paquete
 package main
 
-// en import se coloca todos los paquetes que se importan
-// cuando se importan multiples paquetes se utilizan parentesis ()
 import "fmt"
 
-// func main es el punto de inicio del programa
-// en main se llama al paquete importado
 func main() {
-
 	word := "World"
-	fmt.Println(greeting(username))
-
-	// paquete fmt - funcion Println
 	fmt.Println(greeting(word))
 }
-
 
 func greeting(name string) string {
 	return fmt.Sprint("Hello ", name)
