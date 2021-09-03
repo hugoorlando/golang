b4039743f7df740a9aec4ba47f4c96b300fe8299
happycodingpy <hugo.orlando2030@gmail.com>
2021-09-03 13:40:27 -0300

    Zero value

--- go-programming/05-Zero-Value/main.go
+++ go-programming/05-Zero-Value/main.go
@@ -8,4 +8,10 @@ var z int
 func main() {
 	z = 21
 	fmt.Println(z)
+
+	// Valor cero para: 
+	// booleanos, integers, floats, "" strings vacios
+
+	// nil para:
+	// pointers, funciones, interfaces, slices, channels, maps
 }
