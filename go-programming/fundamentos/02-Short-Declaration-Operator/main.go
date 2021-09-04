e41cdfaeee47cbe1a0812ea355526c1f2671100e
happycodingpy <hugo.orlando2030@gmail.com>
2021-09-04 18:00:10 -0300

    Short declaration operator

---
+++ go-programming/fundamentos/02-Short-Declaration-Operator/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// := es el operador de declaracion corta
+	y := 2 + 4
+	x := 42
+	fmt.Println(x, y)
+
+	name := "Hugo"
+	fmt.Println("Hi",name)
+}
