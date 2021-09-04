304c22d19ed0bf61eb807ea6bdb7343b823ac0d5
happycodingpy <hugo.orlando2030@gmail.com>
2021-09-04 18:02:00 -0300

    Bool type

---
+++ go-programming/fundamentos/09-Bool-Type/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+var b bool
+
+func main() {
+	// Un booleano es un tipo binario que puede tener dos posibles valores: true o false
+
+	// Operador de comparacion
+	// ==, <=, >=, !=, <, >
+	
+	a := true
+	b := false 
+
+	fmt.Println(a == b)
+	fmt.Println(a != b)
+
+	fmt.Printf("El valor b es de tipo: %T\n", b)
+
+	fmt.Println(true && false)
+    fmt.Println(true || false)
+    fmt.Println(!true)
+	
+}
