5640d2fd67088ae52de30b381da9ecfaade57aba
happycodingpy <hugo.orlando2030@gmail.com>
2021-09-06 18:09:49 -0300

    IOTA

--- go-programming/fundamentos/13-IOTA/main.go
+++ go-programming/fundamentos/13-IOTA/main.go
@@ -17,6 +17,12 @@ const (
 )
 
 func (b ByteSize) String() string {
+	// Iota uso de constantes de golang
+
+	// En la definición de constante, iota puede iterar fácilmente un valor de 0 en incrementos de 1, 0,1,2,3,4,5 ...
+	// En este ejemplo, el formato de tamaño de archivo 2 se redondea a la décima potencia, y KB es 1 a la izquierda por 10 bits y MB a la izquierda por 20 bits
+	// Sprintf ("% f", x) en este artículo no causará errores de bucle sin fin porque está definido en el método String, porque% f no intentará llamar a String ()
+	
 	switch {
 	case b >= YB:
 		return fmt.Sprintf("%.2fYB", b/YB)
